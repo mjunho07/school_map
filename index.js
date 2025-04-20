e9553e4f9621e70b987a3c5fb29ab93c92f26b74
junho  myeong <kumgok.s230240@ggh.goe.go.kr>
2025-04-20 15:10:01 +0900

    make api function

--- index.js
+++ index.js
@@ -5,11 +5,37 @@ import express from 'express';
 import nunjucks from 'nunjucks';
 import bodyParser from 'body-parser';
 // import { dirname } from 'path';
-// import path from 'path'
+import path from 'path'
 // import fs from 'fs'
 // import { stringify } from 'querystring';
 import mongoose from 'mongoose';
 
+function getToday(){
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = ("0" + (1 + date.getMonth())).slice(-2);
+    var day = ("0" + date.getDate()).slice(-2);
+
+    return year + month + day;
+}
+async function api() {
+    let i;
+    const response = await fetch(`https://open.neis.go.kr/hub/hisTimetable?ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7530629&ALL_TI_YMD=${'20250421'}&DDDEP_NM=컴퓨터소프트웨어과&GRADE=3&CLASS_NM=9&Type=json&Key=d3343083782d4681922efb5889452f48`);
+    const obj = await response.json();
+    let classList = [];
+    try{
+        for(i = 0;i < obj.hisTimetable[1].row.length;i++)
+            {
+                classList.push(obj.hisTimetable[1].row[i].ITRT_CNTNT);
+            }
+            return classList;
+    }
+    catch(err)
+    {
+        return;
+    }
+    
+}
 
 const app = express();
 
@@ -18,6 +44,9 @@ const __dirname = path.resolve();
 // body parser set
 app.use(bodyParser.urlencoded({ extended: false })); // express 기본 모듈 사용
 app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
 
 // view engine set
 app.set('view engine', 'html'); // main.html -> main(.html)
@@ -28,27 +57,32 @@ nunjucks.configure('views', {
     express: app
 })
 
-const {Schema} = mongoose
+// const {Schema} = mongoose;
 
-// mongoose connect
-mongoose
-    .connect('mongodb+srv://mjh0317:mjhjunho0317mmm7@cluster0.zuq3l.mongodb.net/school_map') // /<> 해당 데이터베이스에 연결됨
-    .then(() => console.log('DB interface'))
-    .catch(e => console.error(e))
+// // mongoose connect
+// mongoose
+//     .connect('mongodb+srv://mjh0317:mjhjunho0317mmm7@cluster0.zuq3l.mongodb.net/school_map') // /<> 해당 데이터베이스에 연결됨
+//     .then(() => console.log('DB interface'))
+//     .catch(e => console.error(e))
 
 
-//mongose set
-const WritingSchema = new Schema({
-    title: String,
-    contents: String,
-    date: {
-        type: Date,
-        default: Date.now,
-    }
+// //mongose set
+// const WritingSchema = new Schema({
+//     title: String,
+//     contents: String,
+//     date: {
+//         type: Date,
+//         default: Date.now,
+//     }
+// })
+
+// const Writing = mongoose.model('Writing', WritingSchema)
+
+
+app.get('/',async (req, res)=>{
+    res.render('index',{list:await api()});
+
 })
-
-const Writing = mongoose.model('Writing', WritingSchema)
-
 
 app.listen(3000, () => {
     console.log('Server is running');
