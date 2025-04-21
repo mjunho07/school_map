cf0d58b1c2b363b7825b41324d337059361fe59e
mjunho07 <kumgok.s230240@ggh.goe.go.kr>
2025-04-22 08:47:35 +0900

    캐시

--- index.js
+++ index.js
@@ -20,7 +20,10 @@ function getToday(){
 }
 async function api() {
     let i;
-    const response = await fetch(`https://open.neis.go.kr/hub/hisTimetable?ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7530629&ALL_TI_YMD=${getToday()}&DDDEP_NM=컴퓨터소프트웨어과&GRADE=3&CLASS_NM=9&Type=json&Key=d3343083782d4681922efb5889452f48&_=${Date.now()}`,{ cache: "no-store" });
+    const response = await fetch(`https://open.neis.go.kr/hub/hisTimetable?ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7530629&ALL_TI_YMD=${getToday()}&DDDEP_NM=컴퓨터소프트웨어과&GRADE=3&CLASS_NM=9&Type=json&Key=d3343083782d4681922efb5889452f48&_=${Date.now()}`,{
+  headers: {
+    "Cache-Control": "no-cache"
+  });
     const obj = await response.json();
     let classList = [];
     try{
