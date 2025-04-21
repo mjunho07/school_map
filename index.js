d95a5cffa33e6e8e2c2d68fb0eab459ad0921952
mjunho07 <kumgok.s230240@ggh.goe.go.kr>
2025-04-22 08:49:10 +0900

    캐시제거

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ async function api() {
     const response = await fetch(`https://open.neis.go.kr/hub/hisTimetable?ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7530629&ALL_TI_YMD=${getToday()}&DDDEP_NM=컴퓨터소프트웨어과&GRADE=3&CLASS_NM=9&Type=json&Key=d3343083782d4681922efb5889452f48&_=${Date.now()}`,{
   headers: {
     "Cache-Control": "no-cache"
-  });
+  }
+});
     const obj = await response.json();
     let classList = [];
     try{
