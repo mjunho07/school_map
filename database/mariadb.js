aa248aa195ed749c175725d1f0eb945484ca0ae0
junho  myeong <kumgok.s230240@ggh.goe.go.kr>
2025-07-02 23:50:39 +0900

    fix

--- database/mariadb.js
+++ database/mariadb.js
@@ -2,10 +2,9 @@ import mariadb from 'mariadb';
 
 const pool = mariadb.createPool({
     host: '127.0.0.1',       
-    user: 'mjh',           
+    user: 'root',           
     password: '0317',         
-    database: 'web309',        
-    connectionLimit : 5
+    database: 'web309'
 });
 
 export default pool;
